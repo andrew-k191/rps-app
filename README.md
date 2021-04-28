f8fa6747d3343f1857a06b6d365fcbd7a275dea5
Andrew Knight <knightandrew21@icloud.com>
2021-04-28 09:10:57 -0700

    final version, complete

---
+++ README.md
@@ -0,0 +1,16 @@
+# Rock, Paper, Scissors(RPS) App
+
+*A simple RPS app written in pure JavaScript, HTML, and CSS*
+
+Check it out <a href="">here</a>
+
+<img src="images/rps_screenshot.png" alt="rock, paper, scissors screenshot">
+
+
+## Summary
+
+This app was relatively simple to construct. The biggest obstacle I encountered was figuring out the game logic and how to render it in JavaScript. As my coding skills improve, I hope in the future to revise this app and make it more aesthetically pleasing.  
+
+## Author
+
+* Andrew Knight - Aspiring Software Developer - (Website) | (LinkedIn)
--- code.js
+++ code.js
@@ -1,118 +1,74 @@
-const rock = document.querySelector('.img1');
-const paper = document.querySelector('.img2');
-const scissors = document.querySelector('.img3');
-const promptText = document.querySelector('.prompt');
-const p1txt = document.querySelector('.p1')
-const p2txt = document.querySelector('.p2');
-const p3txt = document.querySelector('.p3');
+const rock = document.querySelector('.rock');
+const paper = document.querySelector('.paper');
+const scissors = document.querySelector('.scissors');
+const gameText = document.querySelector('.gameText');
+const userPickText = document.querySelector('.userPickText')
+const computerPickText = document.querySelector('.computerPickText');
+const gameOutcomeText = document.querySelector('.gameOutcomeText');
 
-// Computer choice
 const computerChoice = () => {
-    const selections = ['rock', 'paper', 'scissors'];
-    // generate random number 0, 1, or 2
+    const gameOptions = ['rock', 'paper', 'scissors'];
     const randNum = Math.floor(Math.random() * 3);
-    const computerChoice = selections[randNum];
+    const computerChoice = gameOptions[randNum];
     return computerChoice;
 };
 
 const rockMatch = (computerChoice) => {
-    p1txt.textContent = 'Your selection: Rock';
-    p2txt.textContent = `Computer chooses ${(computerChoice.slice(0,1)).toUpperCase() + computerChoice.slice(1)}`;
+    userPickText.textContent = 'Your selection: Rock';
+    computerPickText.textContent = `Computer chooses ${(computerChoice.slice(0,1)).toUpperCase() + computerChoice.slice(1)}`;
     switch (computerChoice) {
         case 'scissors':
-            p3txt.textContent = 'You win! Rock beats Scissors!';
+            gameOutcomeText.textContent = 'You win! Rock beats Scissors!';
             break;
         case 'paper':
-            p3txt.textContent = 'You Lose! Rock loses to Paper';
+            gameOutcomeText.textContent = 'You Lose! Rock loses to Paper';
             break;
         default:
-            p3txt.textContent = `Its a Tie!!`;
+            gameOutcomeText.textContent = `Its a Tie!!`;
     }
 };
 
 const paperMatch = (computerChoice) => {
-    p1txt.textContent = 'Your selection: Paper';
-    p2txt.textContent = `Computer chooses ${(computerChoice.slice(0,1)).toUpperCase() + computerChoice.slice(1)}`;
+    userPickText.textContent = 'Your selection: Paper';
+    computerPickText.textContent = `Computer chooses ${(computerChoice.slice(0,1)).toUpperCase() + computerChoice.slice(1)}`;
     switch (computerChoice) {
         case 'rock':
-            p3txt.textContent = 'You win! Paper beats Rock!';
+            gameOutcomeText.textContent = 'You win! Paper beats Rock!';
             break;
         case 'scissors': 
-            p3txt.textContent = `You Lose! Paper loses to Scissors!`;
+            gameOutcomeText.textContent = `You Lose! Paper loses to Scissors!`;
             break;
         default: 
-            p3txt.textContent = `It's a Tie!`;
+            gameOutcomeText.textContent = `It's a Tie!`;
     }
 };
 
 const scissorsMatch = (computerChoice) => {
-    p1txt.textContent = 'Your selection: Scissors';
-    p2txt.textContent = `Computer chooses ${(computerChoice.slice(0,1)).toUpperCase() + computerChoice.slice(1)}`;
+    userPickText.textContent = 'Your selection: Scissors';
+    computerPickText.textContent = `Computer chooses ${(computerChoice.slice(0,1)).toUpperCase() + computerChoice.slice(1)}`;
     switch (computerChoice) {
         case 'paper':
-            p3txt.textContent = 'You win! Scissors beats Paper!';
+            gameOutcomeText.textContent = 'You win! Scissors beats Paper!';
             break;
         case 'rock':
-            p3txt.textContent = 'You Lose! Scissors loses to Rock!';
+            gameOutcomeText.textContent = 'You Lose! Scissors loses to Rock!';
             break;
         default:
-            p3txt.textContent = `It's a Tie!`;
+            gameOutcomeText.textContent = `It's a Tie!`;
     }
 };
 
-// User selects Rock
 rock.addEventListener('click', () => {
     rockMatch(computerChoice());
-    promptText.textContent = 'To play again just make another selection!!';
+    gameText.textContent = 'To play again just make another selection!!';
 });
 
-// User selects Paper
 paper.addEventListener('click', () => {
     paperMatch(computerChoice());
-    promptText.textContent = 'To play again just make another selection!!';
+    gameText.textContent = 'To play again just make another selection!!';
 });
 
-// User selects Scissors
 scissors.addEventListener('click', () => {
     scissorsMatch(computerChoice());
-    promptText.textContent = 'To play again just make another selection!!';
+    gameText.textContent = 'To play again just make another selection!!';
 });
-
-
-// Change paragraph text when selection is made
-
-// Display user choice:
-// Display computer choice:
-// Display image for winner, none for tie
-
-// console.log(`User choice: ${userChoice()}`);
-// console.log(`Computer choice: ${computerChoice()}`);
-// console.log(determineResult(userChoice(), computerChoice()));
-
-
-
-
-
-// Computer choice will be displayed??
-// Flash the screen green for win, red for loss, yellow for tie
-
-// How will things line or stack up? 
-
-// Rock, paper, and scissors formula calculator, i.e. result
-// (this function will take userChoice and Computer Choice)
-
-// Computer choice involves an array where it has rock paper and scissors corresponding to a number
-
-
-// Option 1: User promt selection (also later account for verification correct information was used)
-// Option 2: click on rock, paper, or scissors, and listen for click event
-
-// Initially we'll start off with 3 images, rock, paper, or scissors
-
-// The winner will have it's image displayed
-
-// For a tie, the image displayed will show a tie
-
-// Also, display a button at the bottom for game restart
-
-// Task: Once user selects, computer chooses randomly from the remaining two options
